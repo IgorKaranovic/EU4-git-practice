a660eda2ca6c3ce5261dc9071e1ec651d4de9389
Igor Karanovic <igor.karanovic21@gmail.com>
2021-03-11 21:26:47 +0100

    master commit movie5 does not know about

--- src/main/java/SeingDifference.java
+++ src/main/java/SeingDifference.java
@@ -1,4 +1,5 @@
 public class SeingDifference {
 
-    // this is new line of work
+    // this is new line of work movie4
+    // This is new line of work in master that movie5 does not know about
 }
